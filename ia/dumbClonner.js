99559ea911b6a5b8dd91437a9a2f0e6885458869
GammaNu/Millicent Billette <commiter@1000i100.fr>
2014-09-25 03:38:45 +0200

    IA ajoutée : Au premier message, récupéré l'url de l'ia adverse, créer un worker clone et lui transmettre chaque message puis immiter chacune de ses réponse. Fonctionnel sous firefox hors protocole file.

---
+++ ia/dumbClonner.js
@@ -0,0 +1,24 @@
+var cloneWorker;
+
+if(typeof exports === 'undefined') exports = this;
+
+exports.onmessage = function (event) {
+	if (event.data != null) {
+		var monId = event.data.playerId;
+		if (!cloneWorker) {
+			cloneWorker = new Worker(otherPlayerScript(monId, event.data.data.players));
+			cloneWorker.onmessage = sendCloneMessage;
+		}
+		cloneWorker.postMessage(event.data);
+	} else postMessage("data null");
+};
+function otherPlayerScript(monId, players){
+	if(players[0].id == monId) players.shift();
+	var otherPlayer = players[0];
+	var exploded = otherPlayer.script.split('/');
+	if (exploded[0]=='ia') exploded.shift();
+	return exploded.join('/');
+}
+function sendCloneMessage(event){
+	if (event.data != null) exports.postMessage(event.data);
+}
